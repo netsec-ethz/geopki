a34697f0ea145cc590f083165713f528fdf0c2af
Nico Hauser <me@tyratox.ch>
2023-05-29 23:16:35 +0200

    Update postgres-bitstring-run.py

--- performance/postgres-bitstring-run.py
+++ performance/postgres-bitstring-run.py
@@ -181,12 +181,12 @@ def run_queries(args: ProcessArgs, executed_queries_value: Value, result_count_v
                                 ','.join(["'" + bit_string[:i] + "'" for i in range(0, len(bit_string))]) +
                                 ")) UNION ALL "
                                 f"(SELECT bit_string, certificate_hashes, neighbor_hash, left_child_hash, right_child_hash "
-                                f"("
+                                f"FROM nodes WHERE"
                                 f"bit_string_txt LIKE '{bit_string}%' AND "
                                 # fix altitude for now
                                 f"min_altitude_of_bit_string(bit_string) <= {22767 - args.query_radius} AND "
                                 f"max_altitude_of_bit_string(bit_string) >= {22767 + args.query_radius}"
-                                f")))"
+                                f"))"
                                 for bit_string in bit_strings
                             ])
                         + (") as sq" if args.count_only else "")
