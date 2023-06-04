be678a194162e7fd16b861ae6d82ef3b1eec107c
Nico Hauser <me@tyratox.ch>
2023-06-04 21:07:57 +0200

    Update certificates.sql

--- database/tables/certificates.sql
+++ database/tables/certificates.sql
@@ -1,15 +1,6 @@
--- Table: public.certificates
-
--- DROP TABLE IF EXISTS public.certificates;
-
-CREATE TABLE IF NOT EXISTS public.certificates
+CREATE TABLE IF NOT EXISTS certificates
 (
     certificate_hash bytea NOT NULL,
     certificate bytea,
     CONSTRAINT certificates_pkey PRIMARY KEY (certificate_hash)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.certificates
-    OWNER to postgis_user;
+);
