45b0e5091d7777315047fcb20b76f22ae2cd615d
Nico Hauser <me@tyratox.ch>
2023-06-23 16:41:20 +0200

    Finish first version of throughput measurement

--- cmd/geopki-client-throughput/geopki-client-throughput.go
+++ cmd/geopki-client-throughput/geopki-client-throughput.go
@@ -4,21 +4,24 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"sync"
 	"time"
 
+	"geopki/pkg/bitstring"
 	"geopki/pkg/comm"
 	"geopki/pkg/geometry"
 )
 
 const (
-	F_GROW = 0.1
+	F_GROW                 = 0.1
+	MAX_QUERIES_PER_SECOND = 50
 )
 
 type Query struct {
 	Longitude float64 `json:"longitude"`
 	Latitude  float64 `json:"latitude"`
-	Altitude  float64 `json:"altitude"`
 	Radius    uint64  `json:"radius"`
 }
 
@@ -27,102 +30,187 @@ type Results struct {
 	failedRequests     int
 }
 
+// mutex controlling whether the threads are running
+var runningMutex sync.RWMutex
+var stopTime time.Time
+
+func measureThroughput(
+	queries []*comm.Query,
+	address string,
+	includeCertificates bool,
+	results chan Results,
+) {
+	// keep track of the current query index
+	numQueries := len(queries)
+	i := 0
+
+	successfulRequests := 0
+	failedRequests := 0
+
+	// acquire mutex preventing the main goroutine from terminating
+	runningMutex.RLock()
+
+	for {
+		if time.Now().After(stopTime) {
+			// when time is over, return results to main thread
+			results <- Results{
+				successfulRequests: successfulRequests,
+				failedRequests:     failedRequests,
+			}
+
+			// release running mutex
+			runningMutex.RUnlock()
+
+			// and stop the go routine
+			return
+		}
+
+		// otherwise send query
+		_, _, _, err := comm.QueryMapServer(
+			address,
+			queries[i],
+			includeCertificates,
+		)
+
+		now := time.Now()
+
+		if err != nil {
+			// write error to stderr but continue, might be to overloaded server
+			fmt.Fprintf(os.Stderr, "request failed: %v\n", err)
+
+			if now.Before(stopTime) {
+				failedRequests++
+			}
+		} else if now.Before(stopTime) {
+			successfulRequests++
+		}
+
+		// use a different query next time
+		i = (i + 1) % numQueries
+	}
+}
+
 func main() {
 
 	var address string
+	var queriesInput string
 	var queriesJson string
-	var runningTime float64
+	var runningTime int
+	var threads int
 	var includeCertificates bool
 
 	flag.StringVar(&address, "address", "", "The HTTP address of the server to send the request to")
-	flag.StringVar(&queriesJson, "queries", `[{"longitude":8.5470994,"latitude":47.3762348,"altitude":0,"radius":10}]`, "The json encoded queries")
-	flag.Float64Var(&runningTime, "time", 8, "The number of seconds the program should run")
+	flag.StringVar(&queriesInput, "queries", `[{"longitude":8.5470994,"latitude":47.3762348,"altitude":0,"radius":10}]`, "The json encoded queries")
+	flag.IntVar(&runningTime, "time", 8, "The number of seconds the program should run")
+	flag.IntVar(&threads, "threads", 1, "The number of threads the program should run")
+	flag.BoolVar(&includeCertificates, "include-certificates", false, "Whether to include the certificates")
 	flag.Parse()
 
 	var querySet []*Query
+
+	if _, err := os.Stat(queriesInput); err == nil {
+		// read json from disk
+		content, err := os.ReadFile(queriesInput)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed reading file '%s': %v\n", queriesInput, err)
+			os.Exit(1)
+		}
+
+		queriesJson = string(content)
+	} else {
+		queriesJson = queriesInput
+	}
+
 	err := json.Unmarshal([]byte(queriesJson), &querySet)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "received invalid query set: %v\n", err)
+		fmt.Fprintf(os.Stderr, "received invalid query set '%s': %v\n", queriesInput, err)
 		os.Exit(1)
 	}
 
-	queries := make([]*comm.Query, len(querySet))
+	queryCount := threads * int(runningTime) * MAX_QUERIES_PER_SECOND
+
+	queries := make([]*comm.Query, queryCount)
 	for i, q := range querySet {
-		query, err := comm.NewQuery(q.Longitude, q.Latitude, q.Altitude, q.Radius, F_GROW, &geometry.GdalCircleApproximator{})
+		if i >= queryCount-1 {
+			break
+		}
+
+		// set altitude to 0
+		query, err := comm.NewQuery(q.Longitude, q.Latitude, 0, q.Radius, F_GROW, &geometry.GdalCircleApproximator{})
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed building a query using (%f,%f,%f,%d)\n", q.Longitude, q.Latitude, q.Altitude, q.Radius)
+			fmt.Fprintf(os.Stderr, "failed building a query using (%f,%f,%f,%d)\n", q.Longitude, q.Latitude, 0.0, q.Radius)
 			os.Exit(1)
 		}
+		// and then overwrite min and max altitude to cover the full altitude range
+		query.MinAltitude = 0
+		query.MaxAltitude = int16(bitstring.C_Z)
 
 		queries[i] = query
 	}
 
-	numQueries := len(queries)
-	quit := make(chan bool)
-	results := make(chan Results)
+	results := make(chan Results, threads)
+
+	// acquire write lock blocking any thread from starting
+	runningMutex.Lock()
+
+	if len(queries)%threads != 0 {
+		log.Fatalf("# of queries (%d) is not a multiple of # of threads (%d)\n", len(queries), threads)
+	}
+	queriesPerThread := len(queries) / threads
+
+	// initialize go routines, divide queries slice
+	for i := 0; i < threads; i++ {
+		go measureThroughput(
+			queries[i*queriesPerThread:(i+1)*queriesPerThread],
+			address,
+			includeCertificates,
+			results,
+		)
+	}
 
 	now := time.Now()
-	sleepDuration := time.Duration(runningTime * float64(time.Second))
-	stopTime := now.Add(sleepDuration)
 
-	// start querying
-	go func() {
-		// keep track of the current query index
-		i := 0
+	// set start time to now
+	startTime := now
+	// compute stop time
+	stopTime = startTime.Add(time.Duration(float64(runningTime) * float64(time.Second)))
 
-		successfulRequests := 0
-		failedRequests := 0
+	// release lock starting the threads
+	runningMutex.Unlock()
+	// sleep a second ensuring a context switch s.t. the query threads acquire a read lock
+	time.Sleep(time.Second)
+	// re-acquire write lock, will block until all threads finished and released their read lock
+	runningMutex.Lock()
 
-		for {
-			select {
-			case <-quit:
-				// when quit signal is received, return results to main thread
-				results <- Results{
-					successfulRequests: successfulRequests,
-					failedRequests:     failedRequests,
-				}
+	// collect results = everything in the results channel at the moment
+	successfulRequests := 0
+	failedRequests := 0
+	resultCount := 0
 
-				// and stop quering
-				return
-			default:
-				_, _, _, err := comm.QueryMapServer(
-					address,
-					queries[i],
-					includeCertificates,
-				)
-
-				now := time.Now()
-
-				if err != nil {
-					// write error to stderr but continue, might be to overloaded server
-					fmt.Fprintf(os.Stderr, "request failed: %v\n", err)
-
-					if now.Before(stopTime) {
-						failedRequests++
-					}
-				} else if now.Before(stopTime) {
-					successfulRequests++
-				}
-				// fmt.Printf("received response: %v\n", requests[len(requests)-1])
-
-				// use a different query next time
-				i = (i + 1) % numQueries
-			}
+CollectResults:
+	for {
+		select {
+		// receive results from go routine
+		case res := <-results:
+			successfulRequests += res.successfulRequests
+			failedRequests += res.failedRequests
+			resultCount++
+		default:
+			// if no results are available anymore, stop
+			break CollectResults
 		}
-	}()
+	}
 
-	// sleep in main thread
-	time.Sleep(sleepDuration)
-
-	// stop go routine
-	quit <- true
-	// receive results from go routine
-	res := <-results
+	if resultCount != threads {
+		log.Fatalf("Spawned %d threads but received %d results?!\n", threads, resultCount)
+	}
 
 	fmt.Printf(
-		"%f,%d,%d\n",
+		"%d,%d,%t,%d,%d\n",
+		threads,
 		runningTime,
-		res.successfulRequests,
-		res.failedRequests,
+		includeCertificates,
+		successfulRequests,
+		failedRequests,
 	)
 }
---
+++ performance/throughput/throughput.py
@@ -0,0 +1,185 @@
+from typing import Dict, Any, Union, Optional, List, Tuple
+from shapely import Polygon, Point
+import click
+import pandas as pd
+import numpy as np
+import subprocess
+import os
+import tempfile
+import json
+
+from tqdm import tqdm
+
+FILE_PATH = os.path.realpath(__file__)
+CURRENT_DIR = os.path.dirname(FILE_PATH)
+
+QUERY_RADIUS = 10
+
+TIME_VALUES = [1, 2, 4, 8, 16, 32]
+THREAD_VALUES = [1, 2, 4, 8, 16, 32, 64]
+
+MAX_QUERIES_PER_SECOND = 50
+
+
+class Query:
+    def __init__(
+            self,
+            longitude: float,
+            latitude: float,
+            radius: int,
+    ):
+        self.longitude = longitude
+        self.latitude = latitude
+        self.radius = radius
+
+    def to_json(self) -> dict[str, Any]:
+        return {
+            "longitude": self.longitude,
+            "latitude": self.latitude,
+            "radius": self.radius,
+        }
+
+# https://www.matecdev.com/posts/random-points-in-polygon.html
+
+
+def sample_point_in_polygon(polygon: Polygon) -> tuple[float, float]:
+    minX, minY, maxX, maxY = polygon.bounds
+
+    while True:
+        # rejection sampling
+        sample = Point(np.random.uniform(minX, maxX),
+                       np.random.uniform(minY, maxY))
+        if polygon.contains(sample):
+            return sample.x, sample.y
+
+
+@click.command()
+@click.argument('website_density_path', type=click.Path(exists=True))
+@click.argument('output_path', type=click.Path(exists=False))
+@click.option(
+    '--address',
+    '-a',
+    type=str,
+    default='http://localhost:1234',
+)
+@click.option(
+    '--repetitions',
+    '-r',
+    'repetitions',
+    type=int,
+    # by default do not repeat a single location
+    default=1
+)
+@click.option('--include-certificates', 'include_certificates', flag_value=True, default=False)
+def main(
+    website_density_path: str,
+    output_path: str,
+    address: str,
+    repetitions: int,
+    include_certificates: bool,
+):
+
+    if not os.path.isfile(website_density_path):
+        raise Exception(f"Website density path does to point to a file")
+
+    if not website_density_path.endswith(".parquet"):
+        raise Exception(f"Website density path does to end in '.parquet'")
+
+    df = pd.read_parquet(website_density_path)
+
+    df['polygon'] = df['polygon'].apply(
+        lambda points:
+        Polygon([
+            (p[1], p[0])
+            for p in points
+        ])
+    )
+
+    if os.path.isdir(output_path):
+        raise Exception(f"Output path points to a directory")
+
+    if not output_path.endswith(".csv"):
+        raise Exception(f"Output path extension has to be .csv")
+
+    if os.path.isfile(output_path):
+        f = open(output_path, "a")
+    else:
+        f = open(output_path, "w")
+        f.write(
+            f"threads,time,include_certificates,successful_requests,failed_requests\n"
+        )
+        f.flush()
+
+    # probability 0 if osm_website_element_count == 0
+    df['weight'] = df['osm_website_element_count']
+
+    # very small probability if osm_website_element_count == 0
+    # df['weights'] = df['osm_website_element_count'] + 1
+
+    max_threads = max(THREAD_VALUES)
+    max_time = max(TIME_VALUES)
+    query_count = max_threads * max_time * MAX_QUERIES_PER_SECOND
+
+    print(f"Generating {query_count} queries..")
+
+    sample = df.sample(
+        n=query_count,
+        weights='weight',
+        random_state=1,
+        replace=True
+    )
+
+    # for each sample, sample a point within the polygon
+    sample['sample_point'] = sample['polygon'].apply(sample_point_in_polygon)
+    query_locations: list[
+        tuple[float, float]
+    ] = sample['sample_point'].values
+
+    with tempfile.NamedTemporaryFile() as fp:
+
+        # write queries to temporary file
+        fp.write(
+            json.dumps([
+                Query(longitude=longitude, latitude=latitude,
+                      radius=QUERY_RADIUS).to_json()
+                for (longitude, latitude) in query_locations
+            ]).encode("utf-8")
+        )
+        # flush to disk
+        fp.flush()
+
+        print("Done.")
+
+        total_iterations = len(TIME_VALUES) * len(THREAD_VALUES) * repetitions
+
+        for i, time, threads in tqdm(
+            (
+                (i, time, threads)
+                for time in TIME_VALUES
+                for threads in THREAD_VALUES
+                for i in range(repetitions)
+            ),
+            total=total_iterations
+        ):
+            # print(f"go with {time}, {threads}, {fp.name}")
+            p = subprocess.Popen(
+                [
+                    f"../../dist/geopki-client-throughput",
+                    f"--address={address}",
+                    f"--time={time}",
+                    f"--threads={threads}",
+                    f"--queries={fp.name}",
+                ]
+                + (["--include-certificates"] if include_certificates else []),
+                stdout=subprocess.PIPE,
+                cwd=CURRENT_DIR
+            )
+
+            f.write(p.stdout.read().decode("ascii"))
+            f.flush()
+
+        f.close()
+
+
+if __name__ == '__main__':
+    main()
