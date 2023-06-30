b919c091b4910f7a1c55de09676dcb114022445a
Nico Hauser <me@tyratox.ch>
2023-06-30 10:37:46 +0200

    Update geopki-server.go

--- cmd/geopki-server/geopki-server.go
+++ cmd/geopki-server/geopki-server.go
@@ -1025,13 +1025,13 @@ func (env *EndpointHandlerEnv) getDropIndices(c *gin.Context) {
 	_, err = tx.Exec(
 		c.Request.Context(),
 		// nodes table
-		"DROP INDEX bit_string_bit_idx;"+
-			"DROP INDEX bit_string_len;"+
-			"DROP INDEX bit_string_integer_idx;"+
+		"DROP INDEX IF EXISTS bit_string_bit_idx;"+
+			"DROP INDEX IF EXISTS bit_string_len;"+
+			"DROP INDEX IF EXISTS bit_string_integer_idx;"+
 			"ALTER TABLE nodes DROP CONSTRAINT nodes_pkey;"+
 			// certificates table
-			"DROP INDEX certificate_hash;"+
-			"DROP INDEX certificate_not_valid_after;"+
+			"DROP INDEX IF EXISTS certificate_hash;"+
+			"DROP INDEX IF EXISTS certificate_not_valid_after;"+
 			"ALTER TABLE certificates DROP CONSTRAINT certificates_pkey;",
 	)
 	if err != nil {
