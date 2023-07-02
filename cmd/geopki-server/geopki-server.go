780bb9eaf1f87261116ba534d51ff2578ec74bcc
Nico Hauser <me@tyratox.ch>
2023-07-02 11:06:10 +0200

    Update geopki-server.go

--- cmd/geopki-server/geopki-server.go
+++ cmd/geopki-server/geopki-server.go
@@ -1124,7 +1124,7 @@ func (env *EndpointHandlerEnv) getFinishPartial(c *gin.Context) {
 			"ALTER TABLE IF EXISTS nodes CLUSTER ON bit_string_integer_idx;"+
 			"CLUSTER nodes USING bit_string_integer_idx;"+
 			// certificates table
-			"CREATE UNIQUE INDEX IF NOT EXISTS certificate_hash ON certificates USING hash (certificate_hash);"+
+			"CREATE INDEX IF NOT EXISTS certificate_hash ON certificates USING hash (certificate_hash);"+
 			"CREATE INDEX IF NOT EXISTS certificate_not_valid_after ON certificates USING btree (not_valid_after);"+
 			"ALTER TABLE IF EXISTS certificates CLUSTER ON certificate_not_valid_after;"+
 			"CLUSTER certificates USING certificate_not_valid_after;",
